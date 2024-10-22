3abd2a749c8cb5c92ce0c60130c66e16e9713dd8
Juan Luis Romero <juanluis.romerofresno@mimacom.com>
2024-10-22 16:55:20 +0200

    Deleted socket.io

--- controllers/customer/customer.ts
+++ controllers/customer/customer.ts
@@ -1,55 +1,46 @@
-import { Socket } from "socket.io";
-import { database } from "../../database";
-import { CreateCustomer, Customer } from "./types";
-import debounce from "lodash/debounce";
+import {database} from "../../database";
+import {CreateCustomer, Customer} from "./types";
 
 const TABLE = "customer";
 
 const getCustomers = async (): Promise<Customer[]> => {
-  try {
-    return await database.query(`SELECT * FROM ${TABLE}`);
-  } catch (error) {
-    return [];
-  }
+    try {
+        return await database.query(`SELECT * FROM ${TABLE}`);
+    } catch (error) {
+        return [];
+    }
 };
 
-export const findCustomers = debounce((name: string, socket: Socket) => {
-  try {
-    if(name !== '') {
-      database
-      .query<Customer[]>(
-        `SELECT * FROM "${TABLE}" WHERE LOWER(name) LIKE LOWER('%${name}%')`
-      )
-      .then((response) => {
-        socket.emit("foundedCustomers", response);
-      });
-    } else {
-      socket.emit("foundedCustomers", []);
+const findCustomers = async (name: string) => {
+    try {
+        return await database
+            .query<Customer[]>(
+                `SELECT * FROM "${TABLE}" WHERE LOWER(name) LIKE LOWER('%${name}%')`
+            )
+    } catch (error) {
+        console.error(error);
+        return []
     }
-  } catch (e) {
-    console.error(e);
-    socket.emit("foundedCustomers", []);
-  }
-}, 500);
+}
 
 const createCustomer = async (customer: CreateCustomer) => {
-  try {
-    const keys = Object.keys(customer);
-    const values = Object.values(customer);
-    const response = await database.query<Customer[]>(
-      `INSERT INTO ${TABLE} (${keys.toString()}) VALUES (${values.map(
+    try {
+        const keys = Object.keys(customer);
+        const values = Object.values(customer);
+        const response = await database.query<Customer[]>(
+            `INSERT INTO ${TABLE} (${keys.toString()}) VALUES (${values.map(
         (val) => `'${val}'`
       )}) RETURNING *`
-    );
-    return response;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
+        );
+        return response;
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
 };
 
 export default {
-  getCustomers,
-  createCustomer,
-  findCustomers,
+    getCustomers,
+    createCustomer,
+    findCustomers,
 };
--- routes/customerRouter.ts
+++ routes/customerRouter.ts
@@ -1,25 +1,35 @@
-import { Request, Router } from "express";
+import {Request, Router} from "express";
+
+import {CreateCustomer} from "../controllers/customer/types";
 import customer from "../controllers/customer/customer";
-import { CreateCustomer } from "../controllers/customer/types";
 
 const customerRouter = Router();
 
 customerRouter.get("/", async (_, res) => {
-  const customers = await customer.getCustomers();
+    const customers = await customer.getCustomers();
 
-  res.json(customers);
+    res.json(customers);
 });
 
-customerRouter.post("/", async (req: Request<{}, {}, CreateCustomer>, res) => {
-  if (!req.body.name) {
-    return res.status(400).send();
-  }
-  const result = await customer.createCustomer(req.body);
+customerRouter.get('/name/:name', async (req: Request<{ name: string }>, res) => {
+    if (!req.params.name) return res.status(400).send();
 
-  if (result) {
-    return res.status(201).json(result[0]);
-  }
-  return res.status(500).send();
+    const customers = await customer.findCustomers(req.params.name);
+
+    if (!customers.length) return res.status(404).send();
+    res.json(customers);
+})
+
+customerRouter.post("/", async (req: Request<{}, {}, CreateCustomer>, res) => {
+    if (!req.body.name) {
+        return res.status(400).send();
+    }
+    const result = await customer.createCustomer(req.body);
+
+    if (result) {
+        return res.status(201).json(result[0]);
+    }
+    return res.status(500).send();
 });
 
 export default customerRouter;
